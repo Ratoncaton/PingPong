03821ea560737b3a74ac90a8605efd4235405927
ratoncaton <ratoncaton12@gmail.com>
2024-01-06 23:23:43 +0100

    drawing

--- src/main.cpp
+++ src/main.cpp
@@ -9,7 +9,19 @@ int main(){
 
     while(WindowShouldClose() == false){
         BeginDrawing();
+
+        //Line
+        DrawLine(screenWidth/2, 0, screenWidth/2, screenHeight, GRAY);
+
+        //Drawing Ball
+        DrawCircle(screenWidth/2, screenHeight/2, 20, WHITE);
         
+        //Paddles
+        DrawRectangle(10, screenHeight/2 - 60, 25, 120, WHITE);
+        DrawRectangle(screenWidth - 35, screenHeight/2 - 60, 25, 120, WHITE);
+
+        
+
         EndDrawing();
     }
 
