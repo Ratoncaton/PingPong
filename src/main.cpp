f1fead8bfa1520433404f728499d7ecf206b5639
ratoncaton <ratoncaton12@gmail.com>
2024-01-06 23:33:29 +0100

    moving the ball

--- src/main.cpp
+++ src/main.cpp
@@ -1,20 +1,50 @@
 #include "raylib.h"
 #include <iostream>
 
+class Ball{
+    public:
+        float x, y;
+        int speed_x, speed_y;
+        int radius;
+
+    void Draw(){
+        DrawCircle(x, y, radius, WHITE);
+    }
+
+    void Update(){
+        x += speed_x;
+        y += speed_y;
+    }
+
+};
+
+Ball ball;
+
 int main(){
     const int screenWidth = 1280;
     const int screenHeight = 800;
     InitWindow(screenWidth, screenHeight, "My Pong Game");
     SetTargetFPS(60);
 
+    ball.radius = 20;
+    ball.x = screenWidth/2;
+    ball.y = screenHeight/2;
+    ball.speed_x = 7;
+    ball.speed_y = 7;
+
     while(WindowShouldClose() == false){
         BeginDrawing();
+        
+        //Updating Ball
+        ball.Update();
+        
+        //Clear screen
+        ClearBackground(BLACK);
 
         //Line
         DrawLine(screenWidth/2, 0, screenWidth/2, screenHeight, GRAY);
-
-        //Drawing Ball
-        DrawCircle(screenWidth/2, screenHeight/2, 20, WHITE);
+        
+        ball.Draw();
         
         //Paddles
         DrawRectangle(10, screenHeight/2 - 60, 25, 120, WHITE);
